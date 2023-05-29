432b8a46fe21a7b0901da6088ea886fc45921947
tj <thinupavani@gmail.com>
2023-05-29 07:46:38 +0530

    index button

--- src/components/homeComponents/HeroSection.tsx
+++ src/components/homeComponents/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { Button } from 'reactstrap'
 
@@ -9,7 +10,10 @@ const HeroSection = () => {
         <div className="col-12 col-lg-6 d-flex flex-column justify-content-center padding-left-100px">
           <h2 className='txt-color-gray font-600 font-64px font-inter'>We design Feeling</h2>
           <p className='font-raleway font-400 font-24px'>We Maily Design a user interface that aligns well with your business goals and creates excellent conversion rates.</p>
-          <Button className='font-raleway btn-hero-sec'>Get Start</Button>
+          {/* <Button className='font-raleway btn-hero-sec'>Get Start</Button> */}
+          <Link href={'#'} className=''>
+            <Image src={'/assests/images/get-start-button.png'} alt='' width={200} height={50} className='img-fluid' />
+          </Link>
         </div>
         <div className="col-12 px-lg-0 d-flex justify-content-end align-items-end hero-img-size">
           <div className="col-12 col-lg-4"></div>
