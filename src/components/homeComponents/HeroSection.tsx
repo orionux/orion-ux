1cdaf16909220020fd7f2f774091ca12ba206782
tj <thinupavani@gmail.com>
2023-05-30 06:11:13 +0530

    update

--- src/components/homeComponents/HeroSection.tsx
+++ src/components/homeComponents/HeroSection.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { Button } from 'reactstrap'
+// import { Button } from 'reactstrap'
 
 const HeroSection = () => {
   return (
