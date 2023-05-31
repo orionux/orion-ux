23284a1e9fd551b4e5b6b34204cf9b63bafbc11a
tj <thinupavani@gmail.com>
2023-05-31 07:33:05 +0530

    footer links update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,63 @@ import React from "react";
 type Props = {};
 
 const Footer = (props: Props) => {
+  const companyLinks = [
+    {
+      id: 1,
+      title: "About us",
+      link: "/about",
+    },
+    {
+      id: 2,
+      title: "Our work",
+      link: "/our_work",
+    },
+    {
+      id: 3,
+      title: "Client",
+      link: "/client",
+    },
+    {
+      id: 4,
+      title: "Our Blog",
+      link: "/blog",
+    },
+    {
+      id: 5,
+      title: "Contact Us",
+      link: "/contact",
+    },
+  ];
+
+  const productsLinks = [
+    {
+      id: 1,
+      title: "Graphic Design",
+      link: "#",
+    },
+    {
+      id: 2,
+      title: "UI/UX Design",
+      link: "#",
+    },
+    {
+      id: 3,
+      title: "Web Development",
+      link: "#",
+    },
+    {
+      id: 4,
+      title: "App Development",
+      link: "#",
+    },
+    {
+      id: 5,
+      title: "Web Hosting",
+      link: "#",
+    },
+  ];
+
+
   return (
     <div className="d-flex flex-column  justify-content-center align-items-center flex-lg-row footer-top-border py-4 main-padding padding-x-100px ">
       <div className="col-12 col-lg-8 d-flex flex-column footer-cols mb-3">
@@ -23,7 +80,7 @@ const Footer = (props: Props) => {
           </div>
         </div>
         <div className="d-flex flex-row d-flex flex-row justify-content-center align-items-center">
-        <div className="col-12 col-lg-4">
+          <div className="col-12 col-lg-4">
             <Image
               src={"/assests/images/logo-footer.svg"}
               alt=""
@@ -38,9 +95,22 @@ const Footer = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="col-12 col-lg-4 d-flex flex-column footer-cols mb-3 verticle-line">
-        <div className="d-flex">
-          <h3>Company</h3>
+      <div className="col-12 col-lg-4 d-flex flex-column flex-lg-row footer-cols mb-3 verticle-line">
+        <div className="d-flex flex-column col-12 col-lg-6">
+          <h3 className="font-500 links-title-footer ">Company</h3>
+          {companyLinks.map((item) => (
+              <Link key={item.id} href={item.link} className="footer-links">
+                <p>{item.title}</p>
+              </Link>
+          ))}
+        </div>
+        <div className="d-flex flex-column col-12 col-lg-6">
+          <h3 className="font-500 links-title-footer ">Products</h3>
+          {productsLinks.map((item) => (
+              <div key={item.id} className="footer-links">
+                <p>{item.title}</p>
+              </div>
+          ))}
         </div>
       </div>
     </div>
