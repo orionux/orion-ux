e2e47a873b444b567d1b77a3a012348d83db34e0
tj <thinupavani@gmail.com>
2023-05-31 07:45:49 +0530

    footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,12 +64,12 @@ const Footer = (props: Props) => {
 
   return (
     <div className="d-flex flex-column  justify-content-center align-items-center flex-lg-row footer-top-border py-4 main-padding padding-x-100px ">
-      <div className="col-12 col-lg-8 d-flex flex-column footer-cols mb-3">
-        <div className="d-flex flex-row d-flex flex-row justify-content-center align-items-center">
-          <div className="col-12 col-lg-6">
+      <div className="col-12 col-lg-7 d-flex flex-column footer-cols mb-3">
+        <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+          <div className="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-start align-items-center mb-3 mb-lg-2">
             <h2 className="footer-title font-900">Let&apos;s Talk</h2>
           </div>
-          <div className="col-12 col-lg-6">
+          <div className="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-start align-items-center mb-3 mb-lg-2">
             <Image
               src={"/assests/images/click-large.png"}
               alt=""
@@ -79,8 +79,8 @@ const Footer = (props: Props) => {
             />
           </div>
         </div>
-        <div className="d-flex flex-row d-flex flex-row justify-content-center align-items-center">
-          <div className="col-12 col-lg-4">
+        <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+          <div className="col-12 col-lg-4 d-flex justify-content-center justify-content-lg-start align-items-center mb-3 mb-lg-2">
             <Image
               src={"/assests/images/logo-footer.svg"}
               alt=""
@@ -89,14 +89,14 @@ const Footer = (props: Props) => {
               className="img-fluid"
             />
           </div>
-          <div className="col-12 col-lg-8">
+          <div className="col-12 col-lg-8 d-flex flex-column text-center text-lg-start mb-3 mb-lg-2">
             <p className="link-footer font-500 mb-0">dil.athuko@gmail.com</p>
-            <p className="link-footer font-500">(+94)717697736</p>
+            <p className="link-footer font-500 mb-0">(+94)717697736</p>
           </div>
         </div>
       </div>
-      <div className="col-12 col-lg-4 d-flex flex-column flex-lg-row footer-cols mb-3 verticle-line">
-        <div className="d-flex flex-column col-12 col-lg-6">
+      <div className="col-12 col-lg-5 padding-left-100px d-flex flex-column flex-lg-row footer-cols mb-3 verticle-line">
+        <div className="d-flex flex-column col-12 col-lg-6 px-2 text-center text-lg-start mb-3 mb-lg-2">
           <h3 className="font-500 links-title-footer ">Company</h3>
           {companyLinks.map((item) => (
               <Link key={item.id} href={item.link} className="footer-links">
@@ -104,7 +104,7 @@ const Footer = (props: Props) => {
               </Link>
           ))}
         </div>
-        <div className="d-flex flex-column col-12 col-lg-6">
+        <div className="d-flex flex-column col-12 col-lg-6 px-2  text-center text-lg-start">
           <h3 className="font-500 links-title-footer ">Products</h3>
           {productsLinks.map((item) => (
               <div key={item.id} className="footer-links">
