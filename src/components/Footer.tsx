7f57015dd9afd94b930079eb6b9d43e69c5996d9
tj <thinupavani@gmail.com>
2023-05-31 06:17:32 +0530

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -5,12 +6,42 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div className="d-flex flex-column flex-lg-row footer-top-border py-4 main-padding">
-      <div className="col-12 col-sm-6 col-lg-3 d-flex flex-column footer-cols mb-3">
-        <h3 style={{ visibility: "hidden" }} className="mb-0 d-none d-lg-block">
-          @ 2022 By Dilshan_UX
-        </h3>
-        <p className="mb-0">© 2023 by Orion UX Labs</p>
+    <div className="d-flex flex-column  justify-content-center align-items-center flex-lg-row footer-top-border py-4 main-padding padding-x-100px ">
+      <div className="col-12 col-lg-8 d-flex flex-column footer-cols mb-3">
+        <div className="d-flex flex-row d-flex flex-row justify-content-center align-items-center">
+          <div className="col-12 col-lg-6">
+            <h2 className="footer-title font-900">Let&apos;s Talk</h2>
+          </div>
+          <div className="col-12 col-lg-6">
+            <Image
+              src={"/assests/images/click-large.png"}
+              alt=""
+              width={150}
+              height={150}
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="d-flex flex-row d-flex flex-row justify-content-center align-items-center">
+        <div className="col-12 col-lg-4">
+            <Image
+              src={"/assests/images/logo-footer.svg"}
+              alt=""
+              width={150}
+              height={150}
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-12 col-lg-8">
+            <p className="link-footer font-500 mb-0">dil.athuko@gmail.com</p>
+            <p className="link-footer font-500">(+94)717697736</p>
+          </div>
+        </div>
+      </div>
+      <div className="col-12 col-lg-4 d-flex flex-column footer-cols mb-3 verticle-line">
+        <div className="d-flex">
+          <h3>Company</h3>
+        </div>
       </div>
     </div>
   );
