e18e57e93a28a04a6449a353c9c924dabd8b77b1
tj <thinupavani@gmail.com>
2023-06-06 06:18:49 +0530

    projects-design-update

--- src/components/homeComponents/LatestProjects.tsx
+++ src/components/homeComponents/LatestProjects.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
 import React from "react";
 
 const LatestProjects = () => {
@@ -5,8 +7,8 @@ const LatestProjects = () => {
     <>
       <div className="d-flex bg-color-dark-gray py-5 padding-x-100px">
         <div className="d-flex flex-column flex-lg-row w-100">
-          <div className="col-12 col-lg-4">
-            <div className="d-flex flex-column flex-lg-row">
+          <div className="col-12 col-lg-4 mt-5">
+            <div className="d-flex flex-column flex-lg-row pt-5">
               <div className="col-12 col-lg-10">
                 <h2 className="text-white font-48px font-space-g font-600 space-2">UI/UX Design</h2>
                 <div className="hr-decor"></div>
@@ -33,7 +35,23 @@ const LatestProjects = () => {
             </div>
           </div>
           <div className="col-12 col-lg-8 p-5 position-relative rectangle-bg">
-            <h2 className="font-inter font-700 font-48px">Latest Projects</h2>
+          <h2 className="font-inter font-700 font-42px" style={{color:"#4D4D4D"}}>Latest Projects</h2>
+            <div className="d-flex flex-column flex-lg-row">
+            <div className="col-12 col-lg-3">
+            
+            </div>
+            <div className="col-12 col-lg-9 text-center d-flex flex-column align-items-center ">
+              <h3 className="font-inter font-700 font-32px pt-5" style={{color:'#EDB507'}}>Bowwow.lk</h3>
+              <Image src={'/assests/images/laptop-yellow.png'} alt='' width={400} height={225} />
+              <div className="d-flex flex-column justify-content-start align-items-start">
+              <p className="font-inter font-400 font-14px text-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet lacus, vitae amet, elit. Elit, elit, tortor ipsum quis pellentesque nisl.</p>
+              <div className="hr-gray mt-3 mb-4"></div>
+              <a href="" className="font-18px font-500 font-inter text-start text-dark" style={{textDecoration: 'none'}}>
+              Watch full Project
+              </a>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
