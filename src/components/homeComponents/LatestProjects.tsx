167d688bed9ebeeb4f48f6d797427aaaed34abc9
tj <thinupavani@gmail.com>
2023-06-06 05:37:19 +0530

    projects section update

--- src/components/homeComponents/LatestProjects.tsx
+++ src/components/homeComponents/LatestProjects.tsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const LatestProjects = () => {
-    return (
-        <>
-            <div className="d-flex bg-color-dark-gray py-5 padding-x-100px">
-                <div className="d-flex flex-column flex-lg-row">
-                    <div className="col-12 col-lg-6">
-                        <h2>UI/UX Design</h2>
-                        <div className="hr-decor"></div>
-                        <div className="d-flex flex-column">
-                            <button className='btn-link-latest mb-3'>Digital Art Works</button>
-                            <div className="d-flex flex-col flex-lg-row">
-                                <div className="col-3"></div>
-                                <div className="col-9">
-                                    <button className='btn-link-latest mb-3'>Digital Art Works</button>
-                                </div>
-                            </div>
-                            <div className="d-flex flex-col flex-lg-row">
-                                <div className="col-6"></div>
-                                <div className="col-6">
-                                    <button className='btn-link-latest mb-3'>Digital Art Works</button>
-                                </div>
-                            </div>
-                            <div className="d-flex flex-col flex-lg-row">
-                                <div className="col-9"></div>
-                                <div className="col-3">
-                                    <button className='btn-link-latest mb-3'>Digital Art Works</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-lg-6"></div>
-                </div>
+  return (
+    <>
+      <div className="d-flex bg-color-dark-gray py-5 padding-x-100px">
+        <div className="d-flex flex-column flex-lg-row w-100">
+          <div className="col-12 col-lg-4">
+            <div className="d-flex flex-column flex-lg-row">
+              <div className="col-12 col-lg-10">
+                <h2 className="text-white font-48px font-space-g font-600 space-2">UI/UX Design</h2>
+                <div className="hr-decor"></div>
+              </div>
+              <div className="col-12 col-lg-2"></div>
             </div>
-        </>
-    )
-}
 
-export default LatestProjects
+            <div className="d-flex flex-column flex-lg-row">
+              {/* <div className="col-12 col-lg-3"></div> */}
+              <div className=".col-12 col-lg-10 d-flex flex-column align-items-center align-items-lg-end">
+                <button className="btn-link-latest mb-3 mt-5 space-1 font-inter font-20px ">
+                  Digital Art Works
+                </button>
+                <button className="btn-link-latest mb-3 mt-3 space-2 font-inter font-20px">
+                  Digital Marketing
+                </button>
+                <button className="btn-link-latest mb-3 mt-3 space-3 font-inter font-20px">
+                  Branding
+                </button>
+                <button className="btn-link-latest mb-3 mt-3 space-4 font-inter font-20px">
+                  3D Designs
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-8 p-5 position-relative rectangle-bg">
+            <h2 className="font-inter font-700 font-48px">Latest Projects</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LatestProjects;
