1e99d1f8214543a07e359ecb8ee03d2505da7db8
tj <thinupavani@gmail.com>
2023-06-07 07:56:55 +0530

    bullet update

--- src/components/homeComponents/Feelings.tsx
+++ src/components/homeComponents/Feelings.tsx
@@ -4,45 +4,47 @@ import React, { useState } from "react";
 const Feelings = () => {
   const [selectedItem, setSelectedItem] = useState("UI/UX Design");
 
+  const categories = {
+    "UI/UX Design": [
+      { id: 1, title: "Mobile App Design" },
+      { id: 2, title: "Web Site Design" },
+      { id: 3, title: "Desktop App Design" },
+      { id: 4, title: "Desktop App Design" },
+      { id: 5, title: "Rewamp(RE - design)" },
+    ],
+    "Software Development": [
+      { id: 1, title: "Software Development 1" },
+      { id: 2, title: "Software Development 2" },
+      { id: 3, title: "Software Development 3" },
+      { id: 4, title: "Software Development 4" },
+      { id: 5, title: "Software Development 5" },
+    ],
+    Branding: [
+      { id: 1, title: "Branding 1" },
+      { id: 2, title: "Branding 2" },
+      { id: 3, title: "Branding 3" },
+      { id: 4, title: "Branding 4" },
+      { id: 5, title: "Branding 5" },
+    ],
+    "Digital Marketing": [
+      { id: 1, title: "Digital Marketing 1" },
+      { id: 2, title: "Digital Marketing 2" },
+      { id: 3, title: "Digital Marketing 3" },
+      { id: 4, title: "Digital Marketing 4" },
+      { id: 5, title: "Digital Marketing 5" },
+    ],
+    "Our Products": [
+      { id: 1, title: "Our Products 1" },
+      { id: 2, title: "Our Products 2" },
+      { id: 3, title: "Our Products 3" },
+      { id: 4, title: "Our Products 4" },
+      { id: 5, title: "Our Products 5" },
+    ],
+  };
+
   const handleItemClick = (item: React.SetStateAction<string>) => {
     setSelectedItem(item);
   };
-
-  const uiUxDesign = [
-    { id: 1, title: "Mobile App Design" },
-    { id: 2, title: "Web Site Design" },
-    { id: 3, title: "Desktop App Design" },
-    { id: 4, title: "Desktop App Design" },
-    { id: 5, title: "Rewamp(RE - design)" },
-  ]
-  const softwareDevelopment = [
-    { id: 1, title: "softwareDevelopment 1" },
-    { id: 2, title: "softwareDevelopment 2" },
-    { id: 3, title: "softwareDevelopment 3" },
-    { id: 4, title: "softwareDevelopment 4" },
-    { id: 5, title: "softwareDevelopment 5" },
-  ]
-  const Branding = [
-    { id: 1, title: "Branding 1" },
-    { id: 2, title: "Branding 2" },
-    { id: 3, title: "Branding 3" },
-    { id: 4, title: "Branding 4" },
-    { id: 5, title: "Branding 5" },
-  ]
-  const digitalMarketing = [
-    { id: 1, title: "digitalMarketing 1" },
-    { id: 2, title: "digitalMarketing 2" },
-    { id: 3, title: "digitalMarketing 3" },
-    { id: 4, title: "digitalMarketing 4" },
-    { id: 5, title: "digitalMarketing 5" },
-  ]
-  const ourProducts = [
-    { id: 1, title: "ourProducts 1" },
-    { id: 2, title: "ourProducts 2" },
-    { id: 3, title: "ourProducts 3" },
-    { id: 4, title: "ourProducts 4" },
-    { id: 5, title: "ourProducts 5" },
-  ]
 
   return (
     <div className="d-none d-lg-flex bg-color-white py-5 padding-x-100px feelings-sec">
@@ -50,86 +52,31 @@ const Feelings = () => {
         <div className="col-12 col-lg-6 pe-5 d-flex justify-content-center flex-column text-start vr-line-feelings">
           <h2 className="font-inter font-600 font-32px">WE DESIGN</h2>
           <h2 className="font-inter font-600 font-48px">FEELINGS</h2>
-          <ul
-            className="ps-0 font-inter font-400 font-18px"
-            style={{ listStyle: "none" }}
-          >
-            <li
-              className={`d-flex flex-row align-items-center ${selectedItem === "UI/UX Design" ? "selected" : ""}`}
-              style={{}}
-              onClick={() => handleItemClick("UI/UX Design")}
-            >
-              <div className="linebullet me-3"></div> UI/UX Design
-            </li>
-            <li
-              className={`d-flex flex-row align-items-center ${selectedItem === "Software Development" ? "selected" : ""}`}
-              style={{}}
-              onClick={() => handleItemClick("Software Development")}
-            >
-              <div className="linebullet me-3"></div> Software Development
-            </li>
-            <li
-              className={`d-flex flex-row align-items-center ${selectedItem === "Branding" ? "selected" : ""}`}
-              style={{}}
-              onClick={() => handleItemClick("Branding")}
-            >
-              <div className="linebullet me-3"></div> Branding
-            </li>
-            <li
-              className={`d-flex flex-row align-items-center ${selectedItem === "Digital marketing" ? "selected" : ""}`}
-              style={{}}
-              onClick={() => handleItemClick("Digital marketing")}
-            >
-              <div className="linebullet me-3"></div> Digital marketing
-            </li>
-            <li
-              className={`d-flex flex-row align-items-center ${selectedItem === "Our Products" ? "selected" : ""}`}
-              style={{}}
-              onClick={() => handleItemClick("Our Products")}
-            >
-              <div className="linebullet me-3"></div> Our Products
-            </li>
+          <ul className="ps-0 font-inter font-400 font-18px" style={{ listStyle: "none" }}>
+            {Object.keys(categories).map((category) => (
+              <li
+                key={category}
+                className={`d-flex flex-row align-items-center mb-4 ${selectedItem === category ? "selected" : ""
+                  }`}
+                onClick={() => handleItemClick(category)}
+              >
+                <div className="linebullet me-3"></div>
+                {category}
+              </li>
+            ))}
           </ul>
         </div>
 
         <div className="col-12 col-lg-6 ps-5 d-flex justify-content-center flex-column text-start">
           <div className="d-none d-lg-block" style={{ height: "90px" }}></div>
-          <ul
-            className="ps-0 font-inter font-300 font-16px "
-            style={{ listStyle: "none" }}
-          >
-            {selectedItem === "UI/UX Design" && (
+          <ul className="ps-0 font-inter font-300 font-16px" style={{ listStyle: "none" }}>
+            {categories[selectedItem] && (
               <>
-                {uiUxDesign.map((item) => (
-                  <li key={item.id}>{item.title}</li>
-                ))}
-              </>
-            )}
-            {selectedItem === "Software Development" && (
-              <>
-                {softwareDevelopment.map((item) => (
-                  <li key={item.id}>{item.title}</li>
-                ))}
-              </>
-            )}
-            {selectedItem === "Branding" && (
-              <>
-                {Branding.map((item) => (
-                  <li key={item.id}>{item.title}</li>
-                ))}
-              </>
-            )}
-            {selectedItem === "Digital marketing" && (
-              <>
-                {digitalMarketing.map((item) => (
-                  <li key={item.id}>{item.title}</li>
-                ))}
-              </>
-            )}
-            {selectedItem === "Our Products" && (
-              <>
-                {ourProducts.map((item) => (
-                  <li key={item.id}>{item.title}</li>
+                {categories[selectedItem].map((item: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }) => (
+                  <div key={item.id} className="d-flex flex-row align-items-center mb-4">
+                    <Image src={"/assests/images/arrowBullet.png"} alt="" width={30} height={30} className="me-2 bulletVisible" />
+                    <li className="second_list_item">{item.title}</li>
+                  </div>
                 ))}
               </>
             )}
